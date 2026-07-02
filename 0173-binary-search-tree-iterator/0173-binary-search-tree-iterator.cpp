3bd6482bc9ae6cbd7a3fe0bfe11bbf31aee8894c
Yash Kumar Kansal <kansalyash504@gmail.com>
2026-07-02 15:47:36 +0530

    Time: 4 ms (53.01%), Space: 31.9 MB (67.35%) - LeetHub

---
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -0,0 +1,29 @@
+class BSTIterator {
+    stack<TreeNode*> st;
+
+    void pushLeft(TreeNode* node) {
+        while (node) {
+            st.push(node);
+            node = node->left;
+        }
+    }
+
+public:
+    BSTIterator(TreeNode* root) {
+        pushLeft(root);
+    }
+
+    int next() {
+        TreeNode* node = st.top();
+        st.pop();
+
+        if (node->right)
+            pushLeft(node->right);
+
+        return node->val;
+    }
+
+    bool hasNext() {
+        return !st.empty();
+    }
+};
