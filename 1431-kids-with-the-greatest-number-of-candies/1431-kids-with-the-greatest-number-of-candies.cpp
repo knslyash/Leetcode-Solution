0500b2f49418107fbbb5fe8a548bde066b8b2d4f
Yash Kumar Kansal <kansalyash504@gmail.com>
2026-07-25 23:06:59 +0530

    Time: 0 ms (100%), Space: 12.6 MB (26.84%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int m=*max_element(candies.begin(),candies.end());
+        vector<bool> ans;
+        for(int x:candies){
+            if(x+extraCandies>=m){
+                ans.push_back(true);
+            }
+            else{
+                ans.push_back(false);
+            }
+        }
+        return ans;
+    }
+};
