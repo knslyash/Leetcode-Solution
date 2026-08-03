570697ce55dec95c98c254c26aec61c45bbf9815
Yash Kumar Kansal <kansalyash504@gmail.com>
2026-08-03 22:28:46 +0530

    Time: 3 ms (99.18%), Space: 130.1 MB (98.37%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    int n;
-    vector<int> dp;
-    int solve(vector<int>& stones,int i){
-        if(i>=n){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int result=stones[i]-solve(stones,i+1);
-        if(i+1<n){
-            result=max(result,stones[i]+stones[i+1]-solve(stones,i+2));
-        }
-        if(i+2<n){
-            result=max(result,stones[i]+stones[i+1]+stones[i+2]-solve(stones,i+3));
-        }
-        return dp[i]=result;
-    }
+   
+   
     string stoneGameIII(vector<int>& stones) {
+        int n;
         n=stones.size();
-        dp.resize(n+1,-1);
-        int ans=solve(stones,0);
-        if(ans>0) return "Alice";
-        else if(ans<0) return "Bob";
+        
+        int a=0;
+        int b=0;int c=0;
+        for(int i=n-1;i>=0;i--){
+            int result=stones[i]-a;
+            if(i+1<n){
+                result=max(result,stones[i]+stones[i+1]-b);
+            }
+            if(i+2<n){
+                result=max(result,stones[i]+stones[i+1]+stones[i+2]-c);
+            }
+            c=b;
+            b=a;
+            a=result;
+        }
+
+        if(a>0) return "Alice";
+        if(a<0) return "Bob";
         else return "Tie";
     }
 };
