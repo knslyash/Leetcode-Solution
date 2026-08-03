8648774acc823e2123ad7e55c6eb12a58a3a1805
Yash Kumar Kansal <kansalyash504@gmail.com>
2026-08-03 15:40:42 +0530

    Time: 3 ms (64.11%), Space: 11.5 MB (56.89%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> radiant,dire;
+        int n=senate.size();
+        for(int i=0;i<n;i++){
+            if(senate[i]=='D') dire.push(i);
+            else radiant.push(i);
+        }
+
+        while(!radiant.empty() && !dire.empty()){
+            int r=radiant.front(); radiant.pop();
+            int d=dire.front(); dire.pop();
+            if(r<d){
+                radiant.push(r+n);
+            }
+            else{
+                dire.push(d+n);
+            }
+        }
+        return (radiant.empty())?"Dire":"Radiant";
+    }
+};
